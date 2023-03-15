1d4dfa949521e263f00898f3884dba626fca7ea8
laur576w <114057522+laur576w@users.noreply.github.com>
2023-03-15 10:23:37 +0100

    Gave buttons Click functions and a Regex to 1-5

--- Energibarometer/MainWindow.xaml.cs
+++ Energibarometer/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,5 +22,20 @@ namespace Energibarometer {
 		public MainWindow() {
 			InitializeComponent();
 		}
+
+		private void NumberOneToFive(object sender, TextCompositionEventArgs e) {
+			Regex regex = new Regex("[^1-5]+");
+			e.Handled = regex.IsMatch(e.Text);
+		}
+
+		private void Button_UploadPerson_Click(object sender, RoutedEventArgs e) { }
+		private void Button_UploadIncident_Click(object sender, RoutedEventArgs e) { }
+
+		private void Button_UpdatePerson_Click(object sender, RoutedEventArgs e) { }
+		private void Button_UpdateIncident_Click(object sender, RoutedEventArgs e) { }
+
+		private void Button_DeletePerson_Click(object sender, RoutedEventArgs e) { }
+		private void Button_DeleteIncident_Click(object sender, RoutedEventArgs e) { }
 	}
+
 }
